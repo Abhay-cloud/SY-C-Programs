112f91ef09c8dc94fdc4e12aa295f662c4520e24
Jvdsih62 <89521705+Jvdsih62@users.noreply.github.com>
2021-09-22 12:26:46 +0530

    Add files via upload

---
+++ multiply two number.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+int a, b,c;
+
+printf("Enter any two numbers: \n");
+
+scanf("%d %d", &a, &b);
+    
+    c=a*b;
+    
+printf("%d * %d = %d", a, b, c);
+
+return 0;
+
+}
